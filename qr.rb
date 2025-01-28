5e2258aed476beb8d8c695ecd211115d6eb9dfc0
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-01-28 20:07:22 +0000

    created QR codes for wikipedia and wifi

--- qr.rb
+++ qr.rb
@@ -1,2 +1,19 @@
 # Write your solution here!
-pp "Hello, world!"
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qr_code = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qr_code.as_png({ :size => 500})
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
+
+
+# create a QR code to join a wifi
+wifi_qr = RQRCode::QRCode.new("WIFI:T:WPA;S: Survivors crib;P:8edd3a99a48ef111;;")
+png = wifi_qr.as_png({ :size => 500})
+
+# Write the image data to a file
+IO.binwrite("wifi.png", png.to_s)
